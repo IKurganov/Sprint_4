98c55a3786d5e75b7c0134409c6d391e8e7da5f0
Ilya Kurganov <49905632+IKurganov@users.noreply.github.com>
2022-09-14 23:00:09 +0300

    Update ImportantQuestionsTest.java
    Изменение переменной на константу BASE_URL

--- src/test/java/tests/ImportantQuestionsTest.java
+++ src/test/java/tests/ImportantQuestionsTest.java
@@ -10,7 +10,7 @@ public class ImportantQuestionsTest extends TestBase {
     @Test
     public void checkAnswerAboutPrice(){
         MainPage mainPage = new MainPage(driver);
-        mainPage.open(baseUrl);
+        mainPage.open(BASE_URL);
         String answer = mainPage.clickOnQuestionAndGiveAnswer("Сколько это стоит? И как оплатить?");
         Assert.assertEquals("Неправильный ответ на вопрос о цене",
                 "Сутки — 400 рублей. Оплата курьеру — наличными или картой.",answer);
@@ -19,7 +19,7 @@ public class ImportantQuestionsTest extends TestBase {
     @Test
     public void checkAnswerAboutQuantity(){
         MainPage mainPage = new MainPage(driver);
-        mainPage.open(baseUrl);
+        mainPage.open(BASE_URL);
         String answer = mainPage.clickOnQuestionAndGiveAnswer("Хочу сразу несколько самокатов! Так можно?");
         Assert.assertEquals("Неправильный ответ на вопрос о кол-ве",
                 "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.",answer);
@@ -28,7 +28,7 @@ public class ImportantQuestionsTest extends TestBase {
     @Test
     public void checkAnswerAboutTime(){
         MainPage mainPage = new MainPage(driver);
-        mainPage.open(baseUrl);
+        mainPage.open(BASE_URL);
         String answer = mainPage.clickOnQuestionAndGiveAnswer("Как рассчитывается время аренды?");
         Assert.assertEquals("Неправильный ответ на вопрос о времени",
                 "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.",
@@ -38,7 +38,7 @@ public class ImportantQuestionsTest extends TestBase {
     @Test
     public void checkAnswerAboutToday(){
         MainPage mainPage = new MainPage(driver);
-        mainPage.open(baseUrl);
+        mainPage.open(BASE_URL);
         String answer = mainPage.clickOnQuestionAndGiveAnswer("Можно ли заказать самокат прямо на сегодня?");
         Assert.assertEquals("Неправильный ответ на вопрос об аренде на сегодня",
                 "Только начиная с завтрашнего дня. Но скоро станем расторопнее.",answer);
@@ -47,7 +47,7 @@ public class ImportantQuestionsTest extends TestBase {
     @Test
     public void checkAnswerAboutNotStandartSituations(){
         MainPage mainPage = new MainPage(driver);
-        mainPage.open(baseUrl);
+        mainPage.open(BASE_URL);
         String answer = mainPage.clickOnQuestionAndGiveAnswer("Можно ли продлить заказ или вернуть самокат раньше?");
         Assert.assertEquals("Неправильный ответ на вопрос о разных ситуациях с заказом",
                 "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.",
@@ -57,7 +57,7 @@ public class ImportantQuestionsTest extends TestBase {
     @Test
     public void checkAnswerAboutScooterCharging(){
         MainPage mainPage = new MainPage(driver);
-        mainPage.open(baseUrl);
+        mainPage.open(BASE_URL);
         String answer = mainPage.clickOnQuestionAndGiveAnswer("Вы привозите зарядку вместе с самокатом?");
         Assert.assertEquals("Неправильный ответ на вопрос о зарядке для самоката",
                 "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.",
@@ -67,7 +67,7 @@ public class ImportantQuestionsTest extends TestBase {
     @Test
     public void checkAnswerAboutOrderCancelling(){
         MainPage mainPage = new MainPage(driver);
-        mainPage.open(baseUrl);
+        mainPage.open(BASE_URL);
         String answer = mainPage.clickOnQuestionAndGiveAnswer("Можно ли отменить заказ?");
         Assert.assertEquals("Неправильный ответ на вопрос об отмене заказа",
                 "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.",
@@ -77,7 +77,7 @@ public class ImportantQuestionsTest extends TestBase {
     @Test
     public void checkAnswerAboutDistantDelivery(){
         MainPage mainPage = new MainPage(driver);
-        mainPage.open(baseUrl);
+        mainPage.open(BASE_URL);
         String answer = mainPage.clickOnQuestionAndGiveAnswer("Я жизу за МКАДом, привезёте?");
         Assert.assertEquals("Неправильный ответ на вопрос о доставке за МКАД",
                 "Да, обязательно. Всем самокатов! И Москве, и Московской области.",
